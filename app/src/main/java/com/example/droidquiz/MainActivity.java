3561badba38e6d34a9e545a65e735a0fd9c4f991
Samip <samipgnyawali15@gmail.com>
2020-07-25 23:37:01 +0545

    Updated

--- app/src/main/java/com/example/droidquiz/MainActivity.java
+++ app/src/main/java/com/example/droidquiz/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.droidquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.udacity.example.droidtermsprovider.DroidtermExampleContract;
 
 public class MainActivity extends AppCompatActivity {
     private Button mButton;
+    private TextView mDefinitiontv,mWordTv;
+    // cursor data
+    private Cursor mData;
+    private int mDefCol,mWordCol;
     // current state
     private int mCurrentState;
     // hidden or shown
@@ -18,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.button_next);
+        mDefinitiontv = findViewById(R.id.text_view_definition);
+        mWordTv = findViewById(R.id.text_view_word);
+        // database operation to get cursor or data from the content provider
+        new WordFetchTask().execute();
     }
 
     /**
@@ -40,15 +54,56 @@ public class MainActivity extends AppCompatActivity {
      * helps to go to the next word
      */
     private void nextWord() {
-        mButton.setText(getString(R.string.show_defination));
-        mCurrentState = STATE_HIDDEN;
+        if (mData!=null){
+            if (!mData.moveToNext()){
+                mData.moveToFirst();
+            }
+            mDefinitiontv.setVisibility(View.INVISIBLE);
+            mButton.setText(getString(R.string.show_defination));
+            // get the data
+            mWordTv.setText(mData.getString(mWordCol));
+            mDefinitiontv.setText(mData.getString(mDefCol));
+            mCurrentState = STATE_HIDDEN;
+        }
+
     }
 
     /**
      * helps to show the definition of the word
      */
     private void showDefinition() {
-        mButton.setText(getString(R.string.next_word));
-        mCurrentState = STATE_SHOWN;
+        if (mData!=null){
+            mDefinitiontv.setVisibility(View.VISIBLE);
+            mButton.setText(getString(R.string.next_word));
+            mCurrentState = STATE_SHOWN;
+        }
+
+    }
+    //-----------------------------CONTENT PROVIDER CONNECTION---------------
+
+    /**
+     * Async Task For Getting The Data From Content Provider
+     */
+    public class WordFetchTask extends AsyncTask<Void,Void, Cursor>{
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            // content resolver : gets the cursor from content provider we need query Uri toit
+            ContentResolver resolver = getContentResolver();
+            // uri pass
+            Cursor cursor = resolver.query(DroidtermExampleContract.CONTENT_URI,null,null,null,null);
+            return cursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+            mData = cursor;
+            // word and definition
+            mDefCol = mData.getColumnIndex(DroidtermExampleContract.COLUMN_DEFINATION);
+            mWordCol = mData.getColumnIndex(DroidtermExampleContract.COLUMN_WORD);
+            // get the next word
+            nextWord();
+        }
     }
 }
--- droidtermprovider/src/main/java/com/udacity/example/droidtermsprovider/DroidtermExampleContract.java
+++ droidtermprovider/src/main/java/com/udacity/example/droidtermsprovider/DroidtermExampleContract.java
@@ -11,14 +11,14 @@ public class DroidtermExampleContract implements BaseColumns {
     public static final String CONTENT_AUTHORITY = "com.example.udacity.droidtermsexample";
     // ------------------------------- CONTENTS------------------------------------
     // if we have to access content from content provider :we have define uri
-    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     //paths from the uri
-    private static final String PATH_TERMS = "terms";
+    public static final String PATH_TERMS = "terms";
     // content uri:full list of definition & items
-    private static final Uri CONTENT_URI  = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TERMS).build();
+    public static final Uri CONTENT_URI  = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TERMS).build();
     // content type:list of definition
-    private static final String CONTENT_TYPE =
+    public static final String CONTENT_TYPE =
             ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TERMS;
     // content item type:single item definition
     public static final String CONTENT_ITEM_TYPE =
@@ -26,20 +26,20 @@ public class DroidtermExampleContract implements BaseColumns {
 
     //===============================DATABASE ===================================================
         // sql type content provider
-    private static final int DATABASE_VERSION  = 1;
-    private static final String DATABASE_NAME  = "terms";
+    public static final int DATABASE_VERSION  = 1;
+    public static final String DATABASE_NAME  = "terms";
 
     // table name
-    private static final String TERMS_TABLE  = "term_entries";
+    public static final String TERMS_TABLE  = "term_entries";
 
-    private static final String COLUMN_WORD  = "word";
-    private static final String COLUMN_DEFINATION  = "definition";
-    private static final String [] COLUMNS  = {_ID,COLUMN_WORD,COLUMN_DEFINATION};
+    public static final String COLUMN_WORD  = "word";
+    public static final String COLUMN_DEFINATION  = "definition";
+    public static final String [] COLUMNS  = {_ID,COLUMN_WORD,COLUMN_DEFINATION};
 
     // indexes
-    private static final int COLUMN_INDEX_ID = 0;
-    private static final int COLUMN_INDEX_WORD = 1;
-    private static final int COLUMN_INDEX_DEFINITION = 2;
+    public static final int COLUMN_INDEX_ID = 0;
+    public static final int COLUMN_INDEX_WORD = 1;
+    public static final int COLUMN_INDEX_DEFINITION = 2;
 
     /**
      * This Method Creates A Uri Link Between Our App And Droid App And Get Word Definition by id
